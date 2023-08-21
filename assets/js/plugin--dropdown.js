32795680edb0b1b7ea3bebbd8cd23fa45b8b703d
Matthew Stone <junkmail@sandboxstudio.net>
2023-08-21 12:31:10 -0500

    refined mobile nav

--- assets/js/plugin--dropdown.js
+++ assets/js/plugin--dropdown.js
@@ -21,7 +21,7 @@ var Dropdown = function (config) {
   this.menu = config.menu;
   this.event = config.event || 'hover';
   this.pane = document.getElementById(this.menu);
-  // console.log(this.pane);
+  this.trigger = document.getElementById(this.hook); 
 }
 
 
@@ -44,7 +44,6 @@ Dropdown.prototype.modifyHooks = function(hook, func) {
 
 Dropdown.prototype.addListener = function(event, elem) {
   if(event == 'hover'){
-    console.log("hover?: "+event);
     elem.addEventListener("mouseenter", function(e) {
       if (e.target.closest("#"+this.hook)) {
         this.toggleMenu(elem);
@@ -65,15 +64,9 @@ Dropdown.prototype.addListener = function(event, elem) {
       } 
     }.bind(this), false);
   } else {
-    console.log("click?: "+event);
-    elem.addEventListener("click", function(e) {
-      // if (e.target.closest("#"+this.hook)) {
-        this.toggleMenu(elem);
-      // }
-      // else if (e.target.closest("#"+this.menu)) { return; }
-      // else if (this.pane.classList.contains('js-dropdown-visible')) {
-      //   this.toggleMenu(elem);
-      // } 
+    elem.addEventListener("click", function(e) { 
+      this.toggleMenu(elem); 
+      this.toggleTrigger(elem);
     }.bind(this), false);
   }
 }
@@ -84,6 +77,24 @@ Dropdown.prototype.toggleMenu = function(elem) {
     this.pane.classList.replace('js-dropdown-hidden', 'js-dropdown-visible');
   } else if (this.pane.classList.contains('js-dropdown-visible')) {
     this.pane.classList.replace('js-dropdown-visible', 'js-dropdown-hidden');
+  } 
+  return false;
+}
+
+// toggle trigger classes
+Dropdown.prototype.toggleTrigger = function(elem) {
+  console.log(this);
+  if (this.trigger.classList.contains('js-ddtrigger-closed')) {
+    this.trigger.classList.replace('js-ddtrigger-closed', 'js-ddtrigger-open');
+    document.body.className += 'stop-scroll';
+    document.documentElement.className += 'stop-scroll';
+    // document.body.addEventListener('touchmove', function(e){e.preventDefault()}, false);
+    document.body.addEventListener('touchmove', function(e){e.preventDefault()});
+  } else if (this.trigger.classList.contains('js-ddtrigger-open')) {
+    this.trigger.classList.replace('js-ddtrigger-open', 'js-ddtrigger-closed');
+    document.body.classList.remove('stop-scroll');
+    document.documentElement.classList.remove('stop-scroll');
+    document.body.removeEventListener('touchmove', function(e){e.preventDefault()});
   } 
   return false;
 }
